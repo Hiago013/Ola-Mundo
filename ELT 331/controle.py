3a6722681946e3cd3258a94a126df8c369c00934
Hiago de Oliveira Braga Batista <hiagodeoliveirabraga@gmail.com>
2021-10-04 23:40:46 -0300

    Resolved merge conflict by incorporating both suggestions.

--- ELT 331/controle.py
+++ ELT 331/controle.py
@@ -19,12 +19,15 @@ class digital_controller:
         
         self.Gks = num_aux / den_aux
 
+<<<<<<< HEAD
     def X_k(self, k=4, plot=False):
         time , magnitude = impulse_response(self.GkC)
+=======
+    def X_k(self, k=4):
+        time , magnitude = impulse_response(self.Xz)
+>>>>>>> parent of 92396ec (plot, teste_classe)
         for i in range(k):
             print(f'x({i}) = {magnitude[i]}')
-        if plot == True:
-            plt.stem(time[:k], magnitude[:k])
-            plt.grid()
-            plt.show()
-        return time[:k], magnitude[:k]
+        plt.stem(time[:k], magnitude[:k])
+        plt.grid()
+        plt.show()
