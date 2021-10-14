bd5512d3923204ec7b8d4e0bd3a7167f8641dbfe
Hiago de Oliveira Braga Batista <hiagodeoliveirabraga@gmail.com>
2021-10-14 11:51:41 -0300

    integral dupla
    foi modificado os metodos para poder calcular a integral dupla

--- MAT 271/Integrais.py
+++ MAT 271/Integrais.py
@@ -28,12 +28,25 @@ class Integrais:
         return np.float64(t_max)
         
     
-    def trapezio(self, n = 2, dp = 4, erro=0.01):
-        print('*'*15,'Regra do Trapézio', 15*'*')
+    def trapezio(self, n = 2, dp = 4, erro=0.01, dupla=False):
+        x = var('x')
         f = self._f
         a = self._a
         b = self._b
         h = (b - a) / n
+
+        if dupla == True:
+            print('*'*15,'Regra do Trapézio -- Integral Dupla', 15*'*')
+            total = f(x, a) + f(x, b)
+            n_aux = a + h
+            while n_aux < b:
+                total += (2 * f(x, n_aux))
+                n_aux += h
+            y = (h/2 * total)
+            display(y)
+            return Lambda(x, y)
+        
+        print('*'*15,'Regra do Trapézio', 15*'*')
         df2_max = self.erro_trp()
         n_min = (df2_max * (b - a)**3 / (12 * erro))**0.5
         n_min = np.ceil(n_min)
@@ -60,12 +73,31 @@ class Integrais:
         
         return y_round
 
-    def simpson1_3(self, n=2, dp=4, erro=0.01):
-        print('*'*15,'Regra de 1/3 de Simpson', 15*'*')
+    def simpson1_3(self, n=2, dp=4, erro=0.01, dupla = False):
+        x = var('x')
         f = self._f
         a = self._a
         b = self._b
         h = (b - a) / n
+
+        if dupla == True:
+            print('*'*15,'Regra de 1/3 de Simpson -- Integral Dupla', 15*'*')
+            total = f(x, a) + f(x, b)
+            n_aux = a + h
+            k = 1
+
+            while n_aux < b:
+                if k%2 == 0:
+                    total += (2 * f(x, n_aux))
+                else:
+                    total += (4 * f(x, n_aux))
+                k += 1
+                n_aux += h
+            display(h / 3 * (total))
+            return Lambda(x, (h / 3 * (total)))
+            
+        
+        print('*'*15,'Regra de 1/3 de Simpson', 15*'*')
         df4_max = self.erro_spn1_3()
         n_min = (df4_max * (b - a)**5 / (180 * erro))**0.25
         n_min = np.ceil(n_min)
@@ -98,12 +130,31 @@ class Integrais:
         
         return y_round
 
-    def simpson3_8(self, n=3, dp=4, erro=0.01):
-        print('*'*15,'Regra de 3/8 de Simpson', 15*'*')
+    def simpson3_8(self, n=3, dp=4, erro=0.01, dupla=False):
+        x = var('x')
         f = self._f
         a = self._a
         b = self._b
         h = (b - a) / n
+
+        if dupla == True:
+            print('*'*15,'Regra de 3/8 de Simpson -- Integral Dupla', 15*'*')
+            total = f(x, a) + f(x, b)
+            n_aux = a + h
+            k = 1
+            
+            while n_aux < b:
+                if k%3 == 0:
+                    total += (2 * f(x, n_aux))
+                else:
+                    total += (3 * f(x, n_aux))
+                k += 1
+                n_aux += h
+            y = (3 / 8 * h  * (total))
+            display(y)
+            return Lambda(x, y)
+        
+        print('*'*15,'Regra de 3/8 de Simpson', 15*'*')
         df4_max = self.erro_spn1_3()
         n_min = (df4_max * (b - a)**5 / (80 * erro))**0.25
         n_min = np.ceil(n_min)
