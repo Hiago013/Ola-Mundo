af09a0d8665cb528eb1e7f04afae87dba5885905
Hiago de Oliveira Braga Batista <hiagodeoliveirabraga@gmail.com>
2021-10-21 22:48:31 -0300

    Inserido a função plot

--- ELT 331/controleII.py
+++ ELT 331/controleII.py
@@ -2,6 +2,7 @@ from lcapy.discretetime import n, z
 from lcapy import delta, s, t, exp
 from control import tf, step_response, feedback
 import numpy as np
+import matplotlib.pyplot as plt
 
 class digital:
     def __init__(self, Xz = 1, Xk = 1, Gs=1, Gsc=1, tau=1):
@@ -41,11 +42,41 @@ class digital:
         self._Gz = self._Gsc.sample(self._tau)
         return self._Gsc.sample(self._tau)
     
-    def Fz(self, T_max = 10, fb=1):
+    def Fz(self, T_max = 10, fb=1, plot=False):
         fz = feedback(self._Gz, fb)
-        time, mag = step_response(fz, T_max)
+        time, mag = step_response(fz, T_max * self._tau)
         self._fz = fz
+
+        if plot == True:
+            time_plot = np.linspace(0, len(time)-1, 1000)
+            mag_plot = []
+            k = 0
+            for valor in time_plot:
+                if valor-1 > k:
+                    k += 1
+                mag_plot.append(mag[k])
+            mag_plot = np.array(mag_plot)
+            plt.style.use('ggplot')
+            plt.plot(time_plot, mag_plot, linewidth=2, color='red')
+            plt.plot(time_plot, np.ones(len(time_plot)), color='black', alpha=.5, linewidth=2)
+            plt.xlabel('Número de amostras', fontfamily='monospace', fontsize='18')
+            plt.ylabel('Amplitude', fontfamily='monospace', fontsize='18')
+            plt.tight_layout()
+            plt.show()
+
         return fz, time, mag
+    
+
+#gss = 10 / (s + 10*s**2)
+#gsc = tf([10], [10, 1])
+#tau = 2
+#Q16 = digital(Gs=gss, Gsc=gsc, tau = 2)
+#print(Q16.s2z())
+#print(Q16.s2t())
+
+#fz, time, mag = Q16.Fz(14, fb = 0.05)
+#print(mag)
+#display(fz)
 
 
 
