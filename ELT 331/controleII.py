f5ff7675ff7a84f6b20715c56ce22aef0eb7ef14
Hiago de Oliveira Braga Batista <hiagodeoliveirabraga@gmail.com>
2021-10-26 18:18:15 -0300

    save fig

--- ELT 331/controleII.py
+++ ELT 331/controleII.py
@@ -42,7 +42,7 @@ class digital:
         self._Gz = self._Gsc.sample(self._tau)
         return self._Gsc.sample(self._tau)
     
-    def Fz(self, T_max = 10, fb=1, plot=False):
+    def Fz(self, T_max = 10, fb=1, plot=False, save=False):
         fz = feedback(self._Gz, fb)
         time, mag = step_response(fz, T_max * self._tau)
         self._fz = fz
@@ -56,12 +56,17 @@ class digital:
                     k += 1
                 mag_plot.append(mag[k])
             mag_plot = np.array(mag_plot)
+            plt.figure(figsize=(8, 5))
             plt.style.use('ggplot')
             plt.plot(time_plot, mag_plot, linewidth=2, color='red')
             plt.plot(time_plot, np.ones(len(time_plot)), color='black', alpha=.5, linewidth=2)
             plt.xlabel('Número de amostras', fontfamily='monospace', fontsize='18')
             plt.ylabel('Amplitude', fontfamily='monospace', fontsize='18')
             plt.tight_layout()
+
+            if save == True:
+                plt.savefig('fig.png', dpi=600)
+
             plt.show()
 
         return fz, time, mag
@@ -80,24 +85,24 @@ class digital:
 
 
     
+if __name__ == '__main__':
+    gss = 1/(10*s**3 + s**2)
+    gsc = tf(1, [10, 1, 0])
+    print(gsc)
+    tau = .2
+    Q16 = digital(Gs=gss, Gsc=gsc, tau = tau)
+    print(Q16.s2z())
+    print(Q16.s2t())
+    val = Q16.Fz(200, plot=True)
+    print(val[0])
 
-gss = 1/(10*s**3 + s**2)
-gsc = tf(1, [10, 1, 0])
-print(gsc)
-tau = .2
-Q16 = digital(Gs=gss, Gsc=gsc, tau = tau)
-print(Q16.s2z())
-print(Q16.s2t())
-val = Q16.Fz(200, plot=True)
-print(val[0])
+    gcs = ([1, 0.1], [1, 2])
+    K = 8
+    print( Q16.controlador(K, gcs, 4) )
 
-gcs = ([1, 0.1], [1, 2])
-K = 8
-print( Q16.controlador(K, gcs, 4) )
-
-#fz, time, mag = Q16.Fz(14, fb = 0.05)
-#print(mag)
-#display(fz)
+    #fz, time, mag = Q16.Fz(14, fb = 0.05)
+    #print(mag)
+    #display(fz)
 
 
 
